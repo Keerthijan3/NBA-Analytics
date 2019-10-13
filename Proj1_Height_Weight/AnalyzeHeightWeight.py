ce32b6d2de88d28811a8e58e28e6780ea9c05786
Keerthijan <keerthijan_r@gmail.com>
2019-10-13 16:40:23 -0400

    Simplified Code

--- Proj1_Height_Weight/AnalyzeHeightWeight.py
+++ Proj1_Height_Weight/AnalyzeHeightWeight.py
@@ -1,7 +1,10 @@
 '''
-This script will output plots based on player heights and weights over the years. 
-This can shed some insight how the ideal bodytype of NBA players has changed over the years
-To filter by player position, pass it in as an argument. Positions are C, SG, PF, SF, PG
+This script will output plots based on player heights and weights over
+the years.
+This can shed some insight how the ideal bodytype of NBA players has 
+changed over the years
+To filter by player position, pass it in as an argument. Positions 
+are C, SG, PF, SF, PG
 
 '''
 import sys
@@ -9,23 +12,42 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from PIL import Image
+
+
+def Plot(x, y, title, filename, ylabel):
+    '''
+    Function to create plots and save
+
+    '''
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111)
+    ax.plot(x, y, color="#d1ae45")
+    ax.set_facecolor("#2E2E2E")
+    ax.set_title(title, fontsize=16)
+    ax.set_xlabel('Year', labelpad=15, fontsize=12)
+    ax.set_ylabel(ylabel, labelpad=15, fontsize=12)
+    plt.grid(True, color="#93a1a1", alpha=0.1)
+    plt.savefig(filename)
+    plt.close()
+
 players = pd.read_csv('../Data/Players.csv')
 seasons = pd.read_csv('../Data/Seasons_Stats.csv')
 
 try:
     pos = sys.argv[1]
-    seasons = seasons[seasons['Pos']==pos]
+    seasons = seasons[seasons['Pos'] == pos]
     naming = "( " + pos + " )"
 except:
     pos = ""
-    naming=""
+    naming = ""
+
 height_arr = list()
 weight_arr = list()
 year = list()
 stddevh = list()
 stddevw = list()
 for i in range(1950, 2018):
-    season_players = seasons[seasons['Year']==i]
+    season_players = seasons[seasons['Year'] == i]
     season_players = season_players['Player'].values
     player_data = players.loc[players['Player'].isin(season_players)]
     heights = player_data['height'].values
@@ -34,44 +56,20 @@ for i in range(1950, 2018):
     weight_arr.append(np.mean(weights))
     stddevh.append(np.std(heights))
     stddevw.append(np.std(weights))
-    year.append(i)
-    
-fig = plt.figure(figsize = (8,8))
-ax = fig.add_subplot(111)
-ax.plot(year, height_arr, color="#d1ae45")
-ax.set_facecolor("#2E2E2E")
-ax.set_title('Avg Height vs Year for NBA Players'+ naming, fontsize=16)
-ax.set_xlabel('Year', labelpad=15, fontsize=12)
-ax.set_ylabel('Height (cm)', labelpad=15, fontsize=12)
-plt.grid(True, color="#93a1a1", alpha=0.1)
-plt.savefig(pos+'heightvsyear.png')
-
-fig = plt.figure(figsize = (8,8))
-ax = fig.add_subplot(111)
-ax.plot(year, stddevh, color="#d1ae45")
-ax.set_facecolor("#2E2E2E")
-ax.set_title(' Height Standard Deviation vs Year for NBA Players' + naming, fontsize=16)
-ax.set_xlabel('Year', labelpad=15, fontsize=12)
-ax.set_ylabel('Height (cm)', labelpad=15, fontsize=12)
-plt.grid(True, color="#93a1a1", alpha=0.1)
-plt.savefig(pos+'heightstdvsyear.png')
-
-fig = plt.figure(figsize = (8,8))
-ax = fig.add_subplot(111)
-ax.plot(year, stddevw, color="#d1ae45")
-ax.set_facecolor("#2E2E2E")
-ax.set_title('Weight Standard Deviation vs Year for NBA Players' + naming, fontsize=16)
-ax.set_xlabel('Year', labelpad=15, fontsize=12)
-ax.set_ylabel('Weight (kg)', labelpad=15, fontsize=12)
-plt.grid(True, color="#93a1a1", alpha=0.1)
-plt.savefig(pos+'weightstdvsyear.png')
-
-fig = plt.figure(figsize = (8,8))
-ax = fig.add_subplot(111)
-ax.plot(year, weight_arr, color="#d1ae45")
-ax.set_facecolor("#2E2E2E")
-ax.set_title('Avg Weight vs Year for NBA Players' + naming, fontsize=16)
-ax.set_xlabel('Year', labelpad=15, fontsize=12)
-ax.set_ylabel('Weight (kg)', labelpad=15, fontsize=12)
-plt.grid(True, color="#93a1a1", alpha=0.1)
-plt.savefig(pos+'weightvsyear.png')
+    year.append(i)  
+Plot(
+    year, height_arr, 'Avg Height vs Year for NBA Players' + naming,
+    pos+'heightvsyear.png', 'Height(cm)'
+)
+Plot(
+    year, stddevh, 'Height Std. Dev. vs Year for NBA Players' + naming,
+    pos+'heightstdvsyear.png', 'Height (cm)'
+)
+Plot(
+    year, stddevw, 'Weight Std. Devi. vs Year for NBA Players' + naming,
+    pos + 'weightstdvsyear.png', 'Weight (kg)'
+)
+Plot(
+    year, weight_arr, 'Avg Weight vs Year for NBA Players' + naming,
+    pos + 'weightvsyear.png', 'Weight (kg)'
+)
