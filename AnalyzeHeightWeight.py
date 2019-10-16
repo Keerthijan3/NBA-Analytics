5d9f466bd4d4e8cf2a8197e59e10aa8179aa3e37
Keerthijan <keerthijan_r@gmail.com>
2019-10-16 18:16:12 -0400

    Simplified Code

---
+++ AnalyzeHeightWeight.py
@@ -0,0 +1,76 @@
+'''
+This script will output plots based on player heights and weights over
+the years.
+This can shed some insight how the ideal bodytype of NBA players has 
+changed over the years
+To filter by player position, pass it in as an argument. Positions 
+are C, SG, PF, SF, PG
+
+'''
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+def Plot(x, y, title, filename, ylabel):
+    '''
+    Function to create plots and save
+
+    '''
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111)
+    ax.plot(x, y, color="#d1ae45")
+    ax.set_facecolor("#2E2E2E")
+    ax.set_title(title, fontsize=16)
+    ax.set_xlabel('Year', labelpad=15, fontsize=12)
+    ax.set_ylabel(ylabel, labelpad=15, fontsize=12)
+    plt.grid(True, color="#93a1a1", alpha=0.1)
+    plt.savefig(filename)
+    plt.close()
+
+
+players = pd.read_csv('Data/Players.csv')
+seasons = pd.read_csv('Data/Seasons_Stats.csv')
+
+try:
+    pos = sys.argv[1]
+    seasons = seasons[seasons['Pos'] == pos]
+    naming = "( " + pos + " )"
+except:
+    pos = ""
+    naming = ""
+
+height_arr = list()
+weight_arr = list()
+year = list()
+stddevh = list()
+stddevw = list()
+for i in range(1950, 2018):
+    season_players = seasons[seasons['Year'] == i]
+    season_players = season_players['Player'].values
+    player_data = players.loc[players['Player'].isin(season_players)]
+    heights = player_data['height'].values
+    weights = player_data['weight'].values
+    height_arr.append(np.mean(heights))
+    weight_arr.append(np.mean(weights))
+    stddevh.append(np.std(heights))
+    stddevw.append(np.std(weights))
+    year.append(i) 
+Plot(
+    year, height_arr, 'Avg Height vs Year for NBA Players' + naming,
+    pos+'heightvsyear.png', 'Height(cm)'
+)
+Plot(
+    year, stddevh, 'Height Std. Dev. vs Year for NBA Players' + naming,
+    pos+'heightstdvsyear.png', 'Height (cm)'
+)
+Plot(
+    year, stddevw, 'Weight Std. Devi. vs Year for NBA Players' + naming,
+    pos + 'weightstdvsyear.png', 'Weight (kg)'
+)
+Plot(
+    year, weight_arr, 'Avg Weight vs Year for NBA Players' + naming,
+    pos + 'weightvsyear.png', 'Weight (kg)'
+)
---
+++ PlayerAnalysis.py
@@ -0,0 +1,85 @@
+'''
+This script will be used to evaluate how nba players' skillset has changed
+over the years for different positions
+The script must be passed a position (C, SG, SF, PF, PG). It will output
+plots showing the evolution of player playstyles over the years.
+
+'''
+
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+try:
+    pos = sys.argv[1]
+except:
+    print("Position not passed!")
+    sys.exit()
+
+
+def Plot(x, y, title, filename, ylabel):
+    '''
+    Function to create plots and save
+
+    '''
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111)
+    ax.plot(x, y, color="#d1ae45")
+    ax.set_facecolor("#2E2E2E")
+    ax.set_title(title, fontsize=16)
+    ax.set_xlabel('Year', labelpad=15, fontsize=12)
+    ax.set_ylabel(ylabel, labelpad=15, fontsize=12)
+    plt.grid(True, color="#93a1a1", alpha=0.1)
+    plt.savefig(filename)
+    plt.close()
+
+
+players = pd.read_csv('Data/Players.csv')
+seasons = pd.read_csv('Data/Seasons_Stats.csv')
+
+seasons = seasons[seasons['Year'] > 1979]
+seasons_centers = seasons[seasons['Pos'] == pos]
+df = seasons_centers[['Year', 'Player', 'G', 'USG%', '3P', '3PA', '2P', '2PA', 'FT', 'FTA', 'ORB', 'DRB', 'AST', 'STL', 'BLK', 'PTS']]
+threepoint = []
+threepointa = []
+twopoint = []
+twopointa = []
+ft = []
+fta = []
+orb = []
+drb = []
+ast = []
+stl = []
+blk = []
+pts = []
+usg = []
+years = []
+for year in range(1980, 2018):
+    dfyear = df[df['Year'] == year]
+    threepoint.append(np.mean(dfyear['3P'].values/dfyear['G'].values))
+    threepointa.append(np.mean(dfyear['3PA'].values/dfyear['G'].values))
+    twopoint.append(np.mean(dfyear['2P'].values/dfyear['G'].values))
+    twopointa.append(np.mean(dfyear['2PA'].values/dfyear['G'].values))
+    ft.append(np.mean(dfyear['FT'].values/dfyear['G'].values))
+    fta.append(np.mean(dfyear['FTA'].values/dfyear['G'].values))
+    orb.append(np.mean(dfyear['ORB'].values/dfyear['G'].values))
+    drb.append(np.mean(dfyear['DRB'].values/dfyear['G'].values))
+    ast.append(np.mean(dfyear['AST'].values/dfyear['G'].values))
+    stl.append(np.mean(dfyear['STL'].values/dfyear['G'].values))
+    blk.append(np.mean(dfyear['BLK'].values/dfyear['G'].values))
+    pts.append(np.mean(dfyear['PTS'].values/dfyear['G'].values))
+    usg.append(np.mean(dfyear['USG%'].values))
+    years.append(year)
+
+Plot(years, threepoint, 'Three Pointers per Game for ' + pos, pos + 'threep.png', 'Pts/game')
+Plot(years, threepointa, 'Three Pointers Attempted per Game for ' + pos, pos + 'threea.png', 'Pts/game')
+Plot(years, twopoint, 'Two Pointers per Game for ' + pos, pos + 'twop.png', 'Pts/game')
+Plot(years, twopointa, 'Two Pointers Attempted of ' + pos, pos + 'twoa.png', 'Pts/game')
+Plot(years, ft, 'Free Throws per Game for ' + pos, pos + 'ftp.png', 'Pts/game')
+Plot(years, orb, 'Offensive Rebounds per Game for ' + pos, pos + 'orb.png', 'Reb/game')
+Plot(years, drb, 'Defensive Rebounds per Game for ' + pos, pos + 'reb.png', 'Reb/game')
+Plot(years, ast, 'Assists per Game for ' + pos, pos + 'ast.png', 'Ast/game')
+Plot(years, stl, 'Steals per Game for ' + pos, pos + 'stl.png', 'Stl/game') 
+Plot(years, blk, 'Blocks per Game for' + pos, pos + 'blk.png', 'Blk/game')
+Plot(years, pts, 'Points per Game for' + pos, pos + 'ppg.png', 'Pts/game')
